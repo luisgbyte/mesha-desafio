ffb4b4cab5c19f941414dcad7ae9edd0ebc6d347
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-07 16:54:56 -0300

    fetch weather API data by city name

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import {
-  Container, Content, Form, CityInfo, Playlist, MusicCard, Wrapper,
+  Container,
+  Content,
+  Form,
+  CityInfo,
+  Playlist,
+  MusicCard,
+  Wrapper,
 } from './styles';
 
 function Home() {
+  const [weather, setWeather] = useState('');
+  const [city, setCity] = useState('');
+
+  const apiKeyWeather = process.env.REACT_APP_APIKEY_WEATHER;
+
+  const handleChangeCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios
+      .get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          appid: apiKeyWeather,
+          q: city,
+          units: 'imperial',
+        },
+      })
+      .then((res) => {
+        const { data } = res;
+
+        setWeather({
+          city: data.name,
+          temperature: data.main.temp,
+        });
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-alert
+        alert('Ops! ocorreu um erro, tente novamente...', error);
+      });
+  };
+
+  const convertFahrenheitToCelsius = (temperature) => ((temperature - 32) / 1.8000).toFixed(1);
+
   return (
     <Container>
       <Content>
         <h3>VEJA QUAIS MÚSICAS COMBINAM COM O CLIMA DA SUA CIDADE </h3>
 
-        <Form>
-          <input type='text' placeholder='Em que cidade você está?' required />
-          <button type='button' onClick={() => {}}>
+        <Form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Em que cidade você está?'
+            value={city}
+            onChange={handleChangeCity}
+            required
+          />
+          <button type='submit' onClick={() => {}}>
             Buscar
           </button>
         </Form>
 
-        <CityInfo>
-          <span>Brasília 06/09/2021</span>
-          <span>38º graus Celsius</span>
-        </CityInfo>
+        {weather && (
+          <CityInfo>
+            <span>
+              {weather.city}
+              {' '}
+              06/09/2021
+            </span>
+            <span>
+              {convertFahrenheitToCelsius(weather.temperature)}
+              {' '}
+              graus
+              Celsius
+            </span>
+          </CityInfo>
+        )}
 
         <Wrapper>
           <h4>Músicas Sugeridas</h4>
